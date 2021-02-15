65e8841853e26d5b2017bf60607dd6bc7702db6e
TJ Sornet <33141343+tj3407@users.noreply.github.com>
2021-02-15 10:47:37 -0800

    Add radio button exercise

---
+++ Exercises/src/com/company/RadioButtonFrame/RadioButtonTest.java
@@ -0,0 +1,12 @@
+package com.company.RadioButtonFrame;
+
+import javax.swing.*;
+
+public class RadioButtonTest {
+    public static void main(String[] args) {
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        radioButtonFrame.setSize(300, 100);
+        radioButtonFrame.setVisible(true);
+    }
+}
